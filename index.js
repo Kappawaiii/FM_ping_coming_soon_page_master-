31d360be23d466273049a8fbd9a947be856b5634
Aaron Newsom <aaronnewsom@Aarons-Mac-mini.local>
2024-03-09 11:17:20 -0500

    fixed link to html and added the begining of an if statement

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const email = document.getElementById("input");
-console.log(email);
+const button = document.getElementById("notify");
+
+button.onclick = function () {
+  let email = document.getElementById("input").value;
+  console.log(email);
+
+  if (email.includes("@") && email.includes(".com")) {
+    console.log("pass");
+  } else {
+    console.log("fail");
+  }
+};
