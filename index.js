7ae02af880ae7c19142040f81a895a2742e77558
Kappawaiii <91710094+Kappawaiii@users.noreply.github.com>
2024-03-12 07:59:07 -0400

    added varibles for new input and button pass and error classes

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const button = document.getElementById("notify");
-
+const buttonError = document.getElementsByClassName("notifyError");
+const buttonPass = document.getElementsByClassName("notifyPass");
+const passInput = document.getElementsByClassName("pass");
+const errorInput = document.getElementsByClassName("error");
+const failMessage = document.getElementById("errorMessage");
+const input = document.getElementById("input");
 button.onclick = function () {
   let email = document.getElementById("input").value;
   console.log(email);
 
   if (email.includes("@") && email.includes(".com")) {
-    console.log("pass");
+    buttonPass.style.visibility = "visible";
+    passInput.style.visibility = "visible";
+    button.style.visibility = "hidden";
+    input.style.visibility = "hidden";
   } else {
+    failMessage.style.hidden = visible;
     console.log("fail");
   }
 };
