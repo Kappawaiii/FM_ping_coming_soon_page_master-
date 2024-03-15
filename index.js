af56352385993624b82978b1d4ca4ea4183efb7b
Aaron Newsom <aaronnewsom@Aarons-Mac-mini.attlocal.net>
2024-03-15 13:05:24 -0400

    added extra restrictions to pass email and changed effects of the if else statements

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const button = document.getElementById("notify");
-const buttonError = document.getElementsByClassName("notifyError");
-const buttonPass = document.getElementsByClassName("notifyPass");
-const passInput = document.getElementsByClassName("pass");
-const errorInput = document.getElementsByClassName("error");
-const failMessage = document.getElementById("errorMessage");
+const message = document.getElementById("errorMessage");
 const input = document.getElementById("input");
+
 button.onclick = function () {
   let email = document.getElementById("input").value;
   console.log(email);
 
-  if (email.includes("@") && email.includes(".com")) {
-    buttonPass.style.visibility = "visible";
-    passInput.style.visibility = "visible";
-    button.style.visibility = "hidden";
-    input.style.visibility = "hidden";
+  if (email.includes("@")) {
+    console.log("pass");
+    if (
+      email.includes(".com") ||
+      email.includes(".org") ||
+      email.includes(".gov")
+    ) {
+      input.style.borderColor = "rgb(46, 204, 114)";
+      input.style.backgroundColor = "rgb(126, 214, 164)";
+      button.style.borderColor = "rgb(46, 204, 114)";
+      button.style.backgroundColor = "rgb(46, 204, 114)";
+      button.style.color = "white";
+    }
   } else {
-    failMessage.style.hidden = visible;
     console.log("fail");
+    input.style.borderColor = "red";
+    input.style.backgroundColor = "rgb(232, 103, 103)";
+    button.style.borderColor = "red";
+    button.style.backgroundColor = "red";
+    button.style.color = "white";
+    message.style.visibility = "visible";
   }
 };
